58d6e31f199bd1cf0bb5d7c2ab017525cf79a2a3
dmudugam <dineth.m@unb.ca>
2024-09-30 23:44:53 -0300

    Adding Personal Site

--- src/api/googleApiConfig.tsx
+++
@@ -1,4 +0,0 @@
-export const CLIENT_ID = '452481674909-9rj39ou6sf6ri65urd918s8j6lmq75jq.apps.googleusercontent.com';
-export const API_KEY = 'AIzaSyAdxy258ZCWvgnsau8RTPlrBgYwPP6WBa4';
-export const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-export const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
